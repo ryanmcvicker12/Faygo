b31a89d0cefd4dfcdc84af10962ea23e03dfe4cd
Ryan McVicker <ryanmcvicker12@gmail.com>
2021-09-19 19:43:55 -0400

    COMPLETE REDO

--- public/js/submit.js
+++
@@ -1,8 +0,0 @@
-
-//function to test out the submit button
-
-const submit_button = document.getElementById("createAccountButton");
-
-submit_button.onclick = function(){
-	// add the user credentials to a database 
-};
